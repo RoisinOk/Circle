bbdcd74f095999ffd6d79feb7930dfa8f4eb0544
RoisinOk <roisin_ok@hotmail.com>
2015-07-23 19:45:37 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by roisinokeeffe on 23/07/2015.
+ */
+
+//function animateCircle(){
+//    alert("pressed");
+//    $("path123").addClass("drawPath");
+//    alert($("path123").className.value);
+//}
